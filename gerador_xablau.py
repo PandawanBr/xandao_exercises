bb40731b8526c1d12adc69914d6f7d174a5e19bf
Rodrigo Kobayashi <rodrigo.barboza@seniorsolution.com.br>
2019-06-28 09:18:24 -0300

    Melhorias conforme solicitado

--- gerador_xablau.py
+++ gerador_xablau.py
@@ -3,12 +3,23 @@ import random
 
 print("Gerador de Xablau")
 number = int(input("Insira o numero de palavras a aparecer: "))
-
+randomList = []
+stringMessage = ''
 xablau_frases = ['quêêêêêê', 'perc', 'fiforn', 'tipoType', 'énada', 'percAss',
                  'vouBotarNaSuaBranch', 'tranquera', 'canalha', 'xablau', 'gcp',
                  'putz', 'chorando', 'valValor', 'trocaBem', 'caaaraaamba', 'taCerto',
                  'QuatroEvinte', 'naoSePodeGanharTodas', 'aceiteMinhaDerrotaPoisEuJaAceitei']
 
 for x in range(number):
-         print(xablau_frases[random.randint(0, (len(xablau_frases)-1))])
+    randomList.append(xablau_frases[random.randint(0, (len(xablau_frases)-1))])
+
+if len(randomList) > 0:
+    for x in range(len(randomList)):
+        if stringMessage != '':
+            stringMessage += '; {}'.format(randomList[x])
+        else:
+            stringMessage += randomList[x]
+    print(stringMessage)
+else:
+    print("Nenhum item selecionado")
 
